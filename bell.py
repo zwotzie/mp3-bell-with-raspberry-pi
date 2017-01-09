87952b4334c859ae3ec7b7f1c679e6be52de3c7c
Ralf Hense <rh@web.de>
2017-01-09 18:43:19 +0100

    change lines and docs

--- bell.py
+++ bell.py
@@ -10,24 +10,25 @@ Taster_vorne = 14
 Taster_Haustuer_unten = 15
 Taster_Haustuer_oben = 18
 
-Relais_oben = 8
-Relais_unten = 25
-
 Relais_x = 23
 Relais_y = 24
+
+Relais_oben = 8
+Relais_unten = 25
 
 GPIO.setmode(GPIO.BCM)
 # GPIO.setwarnings(False)
 GPIO.setwarnings(True)
-GPIO.setup(Relais_oben, GPIO.OUT)
-GPIO.setup(Relais_unten, GPIO.OUT)
 
-GPIO.setup(Taster_Haustuer_oben, GPIO.IN)
-GPIO.setup(Taster_Haustuer_unten, GPIO.IN)
-GPIO.setup(Taster_vorne, GPIO.IN)
-
-GPIO.output(Relais_oben, GPIO.LOW)
+GPIO.setup(Relais_oben,   GPIO.OUT)
+GPIO.setup(Relais_unten,  GPIO.OUT)
+GPIO.output(Relais_oben,  GPIO.LOW)
 GPIO.output(Relais_unten, GPIO.LOW)
+
+GPIO.setup(Taster_Haustuer_oben,  GPIO.IN)
+GPIO.setup(Taster_Haustuer_unten, GPIO.IN)
+GPIO.setup(Taster_vorne,          GPIO.IN)
+
 
 # if you want to shut down the bell at night
 # adjust your time frame here:
@@ -38,10 +39,15 @@ mp3_file = "ringtones/Klingelton_Trompete_Attacke.mp3"
 
 
 def PlaySound():
-    """ pleae play the mp3 """
+    """ please play the mp3 """
+    # first switch power on to amplifier
     GPIO.output(Relais_unten, GPIO.HIGH)  # switch base of bc 550c
     GPIO.output(Relais_oben,  GPIO.HIGH)  # switch base of bc 550c
+
+    # play sound
     os.system('mpg321 '+mp3_file )
+
+    # switch off power to amplifier
     GPIO.output(Relais_unten, GPIO.LOW)   # switch off again
     GPIO.output(Relais_oben,  GPIO.LOW)   # switch off again
 
@@ -67,6 +73,7 @@ while True:
 
     if 1 in taster_use:
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()), taster_use, checkSleep()
-        PlaySound()
+ #       PlaySound()
     time.sleep(0.2)
+
 
